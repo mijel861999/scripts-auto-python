05ef2c0a9281b71bfc867fc55d621d90878d24dc
Miguel Castillo <miguel.castillo@ibit.pe>
2024-01-26 20:00:45 -0500

    feat: agregando el script que abre todo el proyecto del eccommerce

---
+++ bash/abrir-ecom.bat
@@ -0,0 +1,20 @@
+:: Abrir el proyecto de ecommerce
+@echo off
+
+:: Ruta del directorio
+set "directorio=C:\Users\Usuario\workspace\react\react-e-commerce"
+
+:: Abrir la terminal de Windows 11 en el directorio especificado
+start wt -d "%directorio%"
+
+:: Abrir backend del proyecto de ecommerce
+start "" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Laragon\Laragon.lnk"
+start "" "C:\Users\Usuario\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Postman\Postman.lnk"
+
+:: Ruta del directorio
+set "directorio=C:\Users\Usuario\workspace\php\login-app"
+
+:: Abrir la terminal de Windows 11 en el directorio especificado
+start wt -d "%directorio%"
+
+
---
+++ bash/abrir-front-ecom.bat
@@ -0,0 +1,13 @@
+:: Abrir frontend del proyecto de ecommerce
+@echo off
+
+:: start "" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Laragon\Laragon.lnk"
+:: start "" "C:\Users\Usuario\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Postman\Postman.lnk"
+
+:: Ruta del directorio
+set "directorio=C:\Users\Usuario\workspace\react\react-e-commerce"
+
+:: Abrir la terminal de Windows 11 en el directorio especificado
+start wt -d "%directorio%"
+
+
