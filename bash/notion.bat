4a6a768b20499954275610489706fd438a37582a
Miguel Castillo <miguel.castillo@ibit.pe>
2024-01-27 13:33:40 -0500

    feat: agregando script para abrir notion

---
+++ bash/notion.bat
@@ -0,0 +1,6 @@
+:: Abrir notion y notion calendar
+@echo off
+
+start "" "C:\Users\Usuario\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Notion.lnk"
+start "" "C:\Users\Usuario\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Notion Calendar.lnk"
+
