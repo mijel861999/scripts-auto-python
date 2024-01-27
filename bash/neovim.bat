3453d152a55945b5278e376658fac02fd30f825f
Miguel Castillo <miguel.castillo@ibit.pe>
2024-01-27 17:33:09 -0500

    feat: agregando script que me abre configuración de neovim

---
+++ bash/neovim.bat
@@ -0,0 +1,10 @@
+:: Abre la configuración para neovim 
+@echo off
+
+:: Ruta del directorio
+set "directorio=C:\Users\Usuario\AppData\Local\nvim"
+
+:: Abrir la terminal de Windows 11 en el directorio especificado
+start wt -d "%directorio%"
+
+
